9d063c8e3b9fcd1218417c19677ba67cfb6045bf
Tillmann Vogt <tillk.vogt@gmail.com>
2021-04-17 17:27:45 +0200

    read ip_domain.txt instead of ip.txt

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-This project is a local webserver that replaces dyndns services
+We are polling a "what is my ip" service of 1&1 (myfritz.net) with a cronjob and update the ip at route 53
 
-# Updating the IP
-
-There are several blog post where people describe how they adjust the A-record of their domain name to a changing ip address.
-Instead of polling a "what is my ip" service every minute with a cronjob the fritzbox offers a more elegant solution:
-A dyndns support that can call a webserver.
-We host this webserver ourselves and are not dependent on dyndns providers
-
-You can install this webserver by either
-
-## Run it like a Haskell developer
-
-### Install AWS CLI
+## Install AWS CLI
 
 Assuming Ubuntu
 
@@ -19,7 +8,7 @@ Assuming Ubuntu
 * look up your key in the aws console
 * `aws configure`
 
-### Stack
+## Stack
 
 * `git clone https://github.com/tkvogt/fritzbox-route53`
 * `cd fritzbox-route53`
@@ -27,31 +16,10 @@ Assuming Ubuntu
 * `stack build` in the folder with stack.yaml
 * `./.stack-work/install/x86_64-linux-tinfo6/dac5e107f9affd1b3b8c1797ae748b09b977ec0bb02929e29cbc7affc5ad4f04/8.6.3/bin/fritzbox-route53`
 
-*or*
+## cronjob
 
-## Run with Docker
+./.stack-work/install/x86_64-linux-tinfo6/dac5e107f9affd1b3b8c1797ae748b09b977ec0bb02929e29cbc7affc5ad4f04/8.6.3/bin/cronjob-route53 *.autocompletion.io Z32NAI0V3I6P4A $(dig +short wm9fb25lwlihk5jv.myfritz.net)
 
-TODO: not working yet, aws configure inside docker. How?
-
-* `git clone https://github.com/tkvogt/fritzbox-route53`
-* `cd fritzbox-route53`
-* `sudo docker build . -t  updateip`
-* `sudo docker run updateip`
-
-
-## Fritzbox
-
-Then go to Internet -> Freigaben -> Dyndns
-
-![Fritbox Dynamix DNS](dyndns.webp)
-
-and enter your domain name the zone of your dns and this into the Update-URL:
-
-`http://localhost:8090/update?hostname=<domain>&zoneid=Z32NAI0V3I6P4A&ipv4=<ipaddr>`
-
-## Test
-
-* ping your domain
-* Unplug the cable of the fritzbox, wait until the light switches off
-* Plug it in again to get a new ip address
-* ping your domain, and see the new ip
+crontab -e
+*/2 * * * * ~/cronjob-route53 *.autocompletion.io Z32NAI0V3I6P4A $(dig +short wm9fb25lwlihk5jv.myfritz.net)
+*/2 * * * * ~/cronjob-route53   autocompletion.io Z32NAI0V3I6P4A $(dig +short wm9fb25lwlihk5jv.myfritz.net)
--- src/Main.hs
+++ src/Main.hs
@@ -2,48 +2,65 @@
 module Main where
 
 import Control.Lens
+import Control.Monad(when)
 import Control.Monad.IO.Class(liftIO)
 import Data.List.NonEmpty
 import Data.Maybe(fromMaybe)
+import Data.Text
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as L
 import Network.AWS.Route53.ChangeResourceRecordSets
 import Network.AWS.Route53.Types
 import Network.AWS
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Options.Generic
-import Servant
-import Servant.API
+import Options.Applicative
+import System.Exit (exitFailure, exitSuccess)
 import System.IO
+import Text.PrettyPrint.ANSI.Leijen(text, (<$$>))
+
+
+data SourceOptions = SourceOptions
+  { dom :: Text
+  , rid :: Text
+  , ip :: Text }
+
+source :: Parser SourceOptions
+source = SourceOptions
+  <$> Options.Applicative.argument str (metavar "domain")
+  <*> Options.Applicative.argument str (metavar "amazon_route53_zone_id")
+  <*> Options.Applicative.argument str (metavar "ipv4")
+
+updateIP :: SourceOptions -> IO ()
+updateIP (SourceOptions domain zoneid ipv4) = do
+  lgr <- newLogger Debug stdout
+  env <- newEnv Discover
+  T.appendFile ("ip_" ++ (unpack domain) ++ ".txt") ""
+  oldIP <- T.readFile ("ip_" ++ (unpack domain) ++ ".txt")
+  T.writeFile ("ip_" ++ (unpack domain) ++ ".txt") ipv4
+  when (oldIP /= ipv4)
+       (do runResourceT $ runAWS (env & envLogger .~ lgr) $
+                          within Frankfurt $ send (b domain zoneid ipv4)
+           putStrLn "IP has changed"
+           putStrLn (show (domain, zoneid, ipv4)))
+  return ()
+
 
 b :: Text -> Text -> Text -> ChangeResourceRecordSets
-b rid domain ip =
+b domain rid ip =
  changeResourceRecordSets (ResourceId rid)
    (changeBatch $
      (change Upsert $ resourceRecordSet domain A & rrsTTL .~ Just 60 & rrsResourceRecords .~ (Just (pure (resourceRecord ip)))) :| [])
 
--- call with: http://localhost:8090/update?hostname=test.example.com&zoneid=Z32NAI0V3I6P4A&ipv4=<ipaddr>
--- stack build --file-watch --fast --test --exec="register-homeip-route53 --rid=Z32NAI0V3I6P4A --domain=test.example.com --ip=89.247.99.130"
-
-type Api = "update" :> QueryParam "hostname" Text :>
-                       QueryParam "zoneid" Text :>
-                       QueryParam "ipv4" Text :>
-                       Get '[JSON] Bool
-apiProxy :: Proxy Api
-apiProxy = Proxy
-
-updateIP :: Maybe Text -> Maybe Text -> Maybe Text -> Handler Bool
-updateIP hostname zoneid ipv4 = do
-  lgr <- newLogger Debug stdout
-  env <- newEnv Discover
-  liftIO $ putStrLn (show (hostname, zoneid, ipv4))
-  liftIO $ runResourceT $ runAWS (env & envLogger .~ lgr) $
-                          within Frankfurt $ send (b (fromMaybe "" zoneid) (fromMaybe "" hostname) (fromMaybe "" ipv4))
-  return True
-
-app :: Application
-app = serve apiProxy updateIP
 
 main :: IO ()
 main = do
-  run 8090 app
+  execParser opts >>= updateIP
+  exitSuccess
+ where
+  opts = info (helper <*> source)
+   (   fullDesc
+    <> progDescDoc (Just (
+       (text "") <$$>
+       (text "See cronjob-route53 -h for details.")))
+    <> header "" )
 
+-- cronjob-route53 *.example.com Z32NAI0V3I6P4A $(dig +short wm.myfritz.net)
